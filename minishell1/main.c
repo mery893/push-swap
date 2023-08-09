402e076c4814d24c822ced9f51b88370561a04a8
mery893 <138705519+mery893@users.noreply.github.com>
2023-08-09 17:21:58 +0200

    Add files via upload

--- minishell1/main.c
+++ minishell1/main.c
@@ -6,7 +6,7 @@
 /*   By: mennaji <mennaji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 14:46:51 by mennaji           #+#    #+#             */
-/*   Updated: 2023/08/02 18:37:35 by mennaji          ###   ########.fr       */
+/*   Updated: 2023/08/09 17:20:28 by mennaji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,52 @@
 
 char **ft_extend_matrix(char **matrix, char *new_string)
 {
-	int num_rows;
+	int size;
 	int i;
-	int j;
-	char **new_matrix;
+	char **extended_matrix; //variable to store the extende matrix
 
+	size = 0;
 	i = 0;
-	while(matrix[num_rows])
-		num_rows++;
-	new_matrix = (char**)malloc(sizeof(char*) * (num_rows + 1));
-	while(i++ < num_rows){
-		new_matrix[i] = (char*)malloc(matrix[i] + 1);
-		if(!new_matrix[i]){
-			while(j++ < i)
-				free(new_matrix[j]);
-			free(new_matrix);
-		}
-		ft_strcpy(new_matrix[i], matrix[i]);	
-	}
+	extended_matrix = NULL;
+	while(matrix[size])
+		size++;
+	extended_matrix = (char **)malloc((size + 2) * sizeof(char *));
+	if (!extended_matrix)
+        return NULL;
+    // Copy existing strings from matrix to extended_matrix using a while loop
+    int i = 0;
+    while (i < size)
+	{
+        extended_matrix[i] = ft_strdup(matrix[i]);
+		if (!extended_matrix[i]) {
+            // Failed to allocate memory for the string, free the extended_matrix and return NULL
+            while (i-- > 0)
+                free(extended_matrix[i]);
+            free(extended_matrix);
+            return NULL;
+        }
+        i++;
+    }
+    // Copy the new_string into the last element of the extended_matrix
+    extended_matrix[size] = ft_strdup(new_string);
+    if (!extended_matrix[size])
+	{
+        // Failed to allocate memory for the string, free the extended_matrix and return NULL
+        while (i-- > 0)
+            free(extended_matrix[i]);
+        free(extended_matrix);
+        return NULL;
+    }
+    // Set the next element after the new_string to NULL
+    extended_matrix[size + 1] = NULL;
+    // Free the old matrix and return the extended_matrix
+    free(matrix);
+    return extended_matrix;
 }
+
+char *get_env();
+//set the PWD environment variable with the value of str.
+//The resulting environment variable array is stored back in prompt.envp.
 char **set_or_update(char *var, char *value, char **envp, int n)
 {
 	char *new_entry;
@@ -50,7 +77,8 @@ char **set_or_update(char *var, char *value, char **envp, int n)
 	//if an existing variable entry with the same name of as the new entry is found in the envp array
 	while(envp[i])
 	{
-		if(!ft_strcmp(envp[i], var, n) && envp[i][n] == '='){
+		if(!ft_strcmp(envp[i], var, n) && envp[i][n] == '=')
+		{
 			free(envp[i]);
 			envp[i] = new_entry;
 			return (envp);
@@ -65,9 +93,9 @@ char **set_or_update(char *var, char *value, char **envp, int n)
 t_prompt set_env(t_prompt prompt, char *str, char **av)
 {
 	str = getcwd(NULL, 0); //current  working directory
-	prompt.envp = set_or_update_env("PWD", str, prompt.envp,3);
+	prompt.envp = set_or_update_env("PWD", str, prompt.envp,3); //set the pwd environment
 	free(str);
-
+	str =
 }
 
 int main(int ac, char **av, char **env)
@@ -78,4 +106,5 @@ int main(int ac, char **av, char **env)
 		exit(EXIT_FAILURE);
 	}
 	set_env();
+	handles_error();
 }
