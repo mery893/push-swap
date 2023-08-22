f3a903111d3b5b4f15e4ec974867831a43f9119f
mery893 <138705519+mery893@users.noreply.github.com>
2023-08-22 19:54:00 +0200

    Add files via upload

--- minishell1/builtin.c
+++ minishell1/builtin.c
@@ -6,7 +6,7 @@
 /*   By: mennaji <mennaji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 17:37:35 by mennaji           #+#    #+#             */
-/*   Updated: 2023/08/19 18:57:09 by mennaji          ###   ########.fr       */
+/*   Updated: 2023/08/21 19:39:11 by mennaji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,33 @@ int build_echo(t_mini *node)
 		printf("\n");
 	return (0);
 }
+int is_builtin(t_mini *n)
+{
 
+	int len_cmd;
+
+	if(!n->full_cmd)
+		return (0);
+	if(n->full_cmd && ft_strchr(n->full_cmd, '/') || n->full_path && ft_strchr(n->full_path, '/'));
+		return (0);
+	len_cmd = ft_strlen(n->full_cmd);
+	if(!ft_strncmp(n->full_cmd, "pwd", len_cmd) && len_cmd == 3)
+		return(1);
+	if(!ft_strncmp(n->full_cmd, "cd", len_cmd) && len_cmd == 2)
+		return(1);
+	if(!ft_strncmp(n->full_cmd, "export", len_cmd) && len_cmd == 6)
+		return(1);
+	if(!ft_strncmp(n->full_cmd, "unset", len_cmd) && len_cmd == 5)
+		return(1);
+	if(!ft_strncmp(n->full_cmd, "echo", len_cmd) && len_cmd == 4)
+		return(1);
+	if(!ft_strncmp(n->full_cmd, "exit", len_cmd) && len_cmd == 4)
+		return(1);
+	return (0);
+
+}
 int build_cd(t_prompt *info)
 {
-	char **args;
-	char *value;
+	char *args;
 
-	if(args == NULL)
-	{
-		value = get_env("HOME", info->envp, 4);
-		if(chdir(value) == - 1)
-			return (-1);
-		free(value);
-
-	}
 }
